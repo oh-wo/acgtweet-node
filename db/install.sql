2facecdd1c3c44e3738b8a8b52ec12972145f435
owen Bodley <owen@biomatters.com>
2016-04-20 16:36:57 +1200

    master
     * Added better preloaded data

--- db/install.sql
+++ db/install.sql
@@ -3,9 +3,11 @@ CREATE SCHEMA public;
 
 
 CREATE TABLE users (
-  id       SERIAL PRIMARY KEY,
-  email    TEXT NOT NULL,
-  password TEXT NOT NULL
+  id         SERIAL PRIMARY KEY,
+  email      TEXT NOT NULL,
+  password   TEXT NOT NULL,
+  first_name TEXT,
+  last_name  TEXT
 );
 CREATE INDEX users_username_password ON users USING BTREE (email, password);
 
@@ -31,13 +33,11 @@ CREATE TABLE oauth_clients (
 ALTER TABLE oauth_clients
   ADD CONSTRAINT oauth_clients_pkey PRIMARY KEY (client_id, client_secret);
 
-
-INSERT INTO users (email, password) VALUES ('owen@biomatters.com', 'password');
-
 CREATE TABLE sequences (
   id        SERIAL PRIMARY KEY,
   content   TEXT NOT NULL,
-  author_id INT  NOT NULL
+  author_id INT  NOT NULL,
+  isPublic  BOOL
 );
 ALTER TABLE sequences
   ADD CONSTRAINT sequence_author_fk
@@ -50,3 +50,16 @@ CREATE TABLE shared_sequences (
 ALTER TABLE shared_sequences
   ADD CONSTRAINT shared_sequences_pk
 PRIMARY KEY (sequenceId, userId);
+
+-- Preload users.
+INSERT INTO users (first_name, last_name, email, password) VALUES ('Owen', 'Bodley', 'owen@biomatters.com', 'password');
+INSERT INTO users (first_name, last_name, email, password) VALUES ('Bob', 'Roberts', 'bob@biomatters.com', 'password');
+-- Preload sequences
+INSERT INTO sequences (content, author_id, isPublic) VALUES ('acgtagtgctagcatgat', 1, FALSE);
+INSERT INTO sequences (content, author_id, isPublic) VALUES ('acgtaatgat', 1, FALSE);
+INSERT INTO sequences (content, author_id, isPublic) VALUES ('acgtagtgctagcatgat', 1, FALSE);
+INSERT INTO sequences (content, author_id, isPublic) VALUES ('acgtattttttttttaaa', 2, TRUE);
+INSERT INTO sequences (content, author_id, isPublic) VALUES ('gggggggggggggggggg', 2, FALSE);
+-- Preload shared sequences
+INSERT INTO shared_sequences VALUES (3,1);
+INSERT INTO shared_sequences VALUES (4,1);
